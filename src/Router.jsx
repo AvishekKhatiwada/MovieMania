d59fb87bc6f539ad34d5d4245e3cb5746195c87e
Avishek Khatiwada <Khatiwadaavishek12@gmail.com>
2024-05-21 09:51:59 +0545

    miror fixes

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,19 +1,18 @@
 import Details from './Details';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Firstpage from './Firstpage';
 import Home from './Home';
 
 const Router = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path='/' element={<Firstpage />} />
 				<Route path='/details/:id' element={<Details />} />
 				<Route path='/home' element={<Home />} />
-				{/* <Route path='/search?query=:query' element={<Details />}/> */}
 				<Route path="/search"element={<Home />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
 
